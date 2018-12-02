5bfddc99faa542660c6f657571ab506d45e2ab58
ElioTohme <eliotohme@gmail.com>
2018-12-02 05:32:10 -0800

    init commit

---
+++ cpp/count.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <fstream>
+using namespace std;
+
+
+/**
+ * In the assi we need to read and write to a file therefore we user the iostream and fstream 
+ * and since we will be counting occurences map is the best solution (it will sort the values by keys by default)
+ */
+void writeResultToFiles(map<string, int> words)
+{
+    // open files
+    ofstream result;
+    result.open("result.txt");
+    // write map to file
+    for (map<string, int>::iterator wit = words.begin(); wit != words.end(); ++wit)
+        result << wit->first << ":\t" << wit->second << endl;
+    // close file
+    result.close();
+}
+
+int main()
+{
+    ifstream file;
+    file.open("1513-0.txt");
+    if (!file.is_open())
+        return 1;
+    map<string, int> words;
+    while (file.good())
+    {
+        string s;
+        getline(file, s);
+        int pos = s.find_first_of(' ');
+        if (pos < 0)
+            continue;
+        while (s.size() > 0)
+        {
+            pos = s.find_first_of(' ');
+            if (pos < 0)
+                pos = s.size();
+            string word = s.substr(0, pos);
+            if (word != "")
+                words[word]++;
+            s = s.erase(0, pos + 1);
+        }
+    }
+
+    // write result
+    writeResultToFiles(words);
+
+    return 0;
+}
