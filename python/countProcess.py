4416546fa3537912873c937dddf65a2f1d046791
elio_tohme <eliotohme@gmail.com>
2018-12-03 18:57:58 +0200

    sync with process solution

--- python/countProcess.py
+++ python/countProcess.py
@@ -7,12 +7,12 @@ def count(resultqueue, filepath):
     print("Counting Word Occurence in file " + filepath)
     local_counts = {}
     with open(filepath, 'r') as f:
-        words = [word.strip() for word in f.read().split() if word.isalpha() or word == "'"]
-
+        words = [word.strip() for word in f.read().split() if word.isalpha()]
         for word in words:
-            if word not in local_counts:
-                local_counts[word] = 0
-            local_counts[word] += 1
+            lowercase_word = word.lower()
+            if lowercase_word not in local_counts:
+                local_counts[lowercase_word] = 0
+            local_counts[lowercase_word] += 1
     result_queue.put(local_counts)
 
 def writeToFile(result):
